19afdf7b87ee5d1e5652057e68ff4b8d569c6ec7
Guga Glonti <110097877+GugaGlonti@users.noreply.github.com>
2024-05-02 23:49:33 +0200

    chore: Update console.log configuration

--- src/util/consoleLog.config.ts
+++ src/util/consoleLog.config.ts
@@ -2,13 +2,25 @@ export default function runConfig() {
   console.log('#Enabled: Configuring console.log...');
 
   const cl = console.log;
+  const clw = console.warn;
+  const cle = console.error;
+  const clc = console.clear;
 
   console.log = function (...args: Parameters<typeof console.log>) {
     cl('Logger:', ...args, new Date().toLocaleTimeString());
   };
 
   console.clear = function () {
+    clc();
     cl('Console cleared at', new Date().toLocaleTimeString());
+  };
+
+  console.warn = function (...args: Parameters<typeof console.warn>) {
+    clw('Warning:', ...args, new Date().toLocaleTimeString());
+  };
+
+  console.error = function (...args: Parameters<typeof console.error>) {
+    cle('Error:', ...args, new Date().toLocaleTimeString());
   };
 
   cl(console);
