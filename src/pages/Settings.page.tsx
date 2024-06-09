d9b62f41d251895e7070d843cfb8fe4020ab7097
Guga Glonti <110097877+GugaGlonti@users.noreply.github.com>
2024-06-09 16:33:26 +0200

    Add Dashboard, Profile, and Settings pages

---
+++ src/pages/Settings.page.tsx
@@ -0,0 +1,15 @@
+import { DarkThemeToggle, Flowbite } from 'flowbite-react';
+
+export default function SettingsPage() {
+  return (
+    <>
+      <h1>Settings</h1>
+
+      <h1>TEST</h1>
+
+      <Flowbite>
+        <DarkThemeToggle />
+      </Flowbite>
+    </>
+  );
+}
