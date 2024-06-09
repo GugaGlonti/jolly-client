9647e57aad8953590a0ac7c808dd8dd5ff3bcea6
Guga Glonti <110097877+GugaGlonti@users.noreply.github.com>
2024-06-09 20:42:18 +0200

    minor

--- src/util/Classes.config.tsx
+++ src/util/Classes.config.tsx
@@ -4,8 +4,8 @@ export default function ClassesConfig({ children }: PropsWithChildren) {
   return (
     <div
       className='
-    bg-white dark:bg-gray-800
-    text-gray-900 dark:text-gray-400
+    bg-white        dark:bg-gray-800
+    text-gray-900   dark:text-gray-400
 
     '>
       {children}
