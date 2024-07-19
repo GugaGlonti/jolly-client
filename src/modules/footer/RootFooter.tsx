7a4a52ece49b1ea8c5a3c937d07c8110e3cb93ba
Guga Glonti <110097877+GugaGlonti@users.noreply.github.com>
2024-07-19 14:41:22 +0200

    Add basic footer component

--- src/modules/footer/RootFooter.tsx
+++ src/modules/footer/RootFooter.tsx
@@ -1,3 +1,27 @@
+import Logo from '../../components/Logo';
+
 export default function RootFooter() {
-  return <div>RootFooter</div>;
+  return (
+    <>
+      <footer className='py-6 h-fit bg-gray-50 dark:bg-gray-900'>
+        <div className='my-6 mx-24'>
+          <div className='w-full flex justify-between items-center my-8'>
+            <Logo className='h-16' />
+            <div className='flex gap-8'>
+              <div>About</div>
+              <div>Privacy Policy</div>
+              <div>Licensing</div>
+              <div>Contact</div>
+            </div>
+          </div>
+          <hr className='dark: border-gray-800' />
+          <div className='w-full flex justify-center my-8'>
+            <span>
+              {new Date().getFullYear()} Jolly. All Rights <i>not yet</i> Reserved.
+            </span>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
 }
